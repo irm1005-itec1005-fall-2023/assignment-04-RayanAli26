191db7097069cbb08c816fe74978ef4551c60b1e
RayanAli26 <144714398+RayanAli26@users.noreply.github.com>
2023-11-29 05:46:31 +0000

    Assignment 04

--- script.js
+++ script.js
@@ -11,14 +11,62 @@
 
 // Constants
 const appID = "app";
-const headingText = "To do. To done. ✅";
-
+const form = document.querySelector('form');
+const input = document.querySelector('#new-task');
+const taskList = document.querySelector('#task-list');
+const emptyState = document.querySelector('.empty-state');
 // DOM Elements
 let appContainer = document.getElementById(appID);
 
 //
 // Functions
 //
+let tasks = [];
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const task = {
+    id: Date.now(),
+    text: input.value,
+    completed: false
+  };
+  tasks.push(task);
+  input.value = '';
+  renderTasks();
+});
+
+function renderTasks() {
+  taskList.innerHTML = '';
+  tasks.forEach((task) => {
+    const li = document.createElement('li');
+    li.dataset.id = task.id;
+    li.innerHTML = `
+      <label>
+        <input type="checkbox" ${task.completed ? 'checked' : ''}>
+        <span>${task.text}</span>
+      </label>
+      <button class="delete">Delete</button>
+    `;
+    if (task.completed) {
+      li.classList.add('completed');
+    }
+    li.addEventListener('click', (event) => {
+      if (event.target.tagName === 'BUTTON') {
+        tasks = tasks.filter((t) => t.id !== task.id);
+        renderTasks();
+      } else if (event.target.tagName === 'INPUT') {
+        task.completed = event.target.checked;
+        renderTasks();
+      }
+    });
+    taskList.appendChild(li);
+  });
+  if (tasks.length === 0) {
+    emptyState.style.display = 'block';
+  } else {
+    emptyState.style.display = 'none';
+  }
+}
 
 // Add a heading to the app container
 function inititialise() {
